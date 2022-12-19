9166f847ff732ef3cf1bb53aae026b445f018866
DV <unseedevil@gmail.com>
2022-12-19 19:12:49 +0300

    Task_03

---
+++ Task_03/Program.cs
@@ -0,0 +1,11 @@
+﻿System.Console.Write("Введите четное число: ");
+int n = int.Parse(Console.ReadLine());
+int x = n%2;
+if (x==0)
+{
+    System.Console.WriteLine("Ваше число четное.");
+}
+else
+{
+    System.Console.WriteLine("Вы ввели не четное число.");
+}
