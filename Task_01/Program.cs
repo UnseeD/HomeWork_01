5e5c5aaf6601fa760c9bf84e6e6df9937f198a06
DV <unseedevil@gmail.com>
2022-12-19 17:54:01 +0300

    Task_01

---
+++ Task_01/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.Write("Введите число А: ");
+int numA = int.Parse(Console.ReadLine());
+System.Console.Write("Введите число B: ");
+int numB = int.Parse(Console.ReadLine());
+if (numA > numB)
+{
+    System.Console.WriteLine($"max = {numA} ");
+}
+else 
+{
+    System.Console.WriteLine($"max = {numB} ");
+}
