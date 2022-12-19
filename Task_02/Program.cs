41ac4b06a72a9ae262460a0247e292116cd4e81a
DV <unseedevil@gmail.com>
2022-12-19 18:25:39 +0300

    Task_02

---
+++ Task_02/Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.WriteLine("Enter A: ");
+int numA = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Enter B: ");
+int numB = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Enter C: ");
+int numC = int.Parse(Console.ReadLine());
+int max = numA;
+if (max < numB)
+{
+    max = numB;
+}
+if (max < numC)
+{
+   max = numC;
+}
+
+System.Console.WriteLine($"Ответ: {max}");
