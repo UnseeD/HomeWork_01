c460ea8acbf69894cc6ca90abd2235601d4102a6
DV <unseedevil@gmail.com>
2022-12-19 19:49:55 +0300

    Task_04

---
+++ Task_04/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int count = -n;
+int x = count % 2;
+while (count <= n)
+{
+    if (count < 0)
+    {
+       count = count + 1; 
+    }
+    if (x == 0)
+    {
+        Console.Write($"{count}, ");
+        count = count + 1;       
+    }
+    else
+    {
+       count = count + 1; 
+    }
+}
